7d948d6d4ff983ecb8d188fa4d076e41296915e4
Maureenkamau03 <mkaymaureen@gmail.com>
2023-08-30 16:32:19 +0300

    Added a login form using flask-WTF

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,20 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for 
+from app.forms import LoginForm
 
 @app.route("/")
-@app.route("/shop")
-def index():
-    return render_template('shop.html', title='shop Page')
+@app.route("/home")
+def home():
+    return render_template('index.html', title='shop Page')
 
 @app.route("/register")
 def register():
     return render_template('register.html', title='Register Page')
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', title='Login Page')
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'You are requesting to login in {form.username.data}')
+        return redirect(url_for('index'))   
+    return render_template('login.html', title='Login', form=form)
